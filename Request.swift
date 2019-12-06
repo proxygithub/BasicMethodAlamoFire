e8c5cd7e2e958c0a36ee88d92c3962ab86c87ac4
proxygithub <54106387+proxygithub@users.noreply.github.com>
2019-12-06 08:31:26 +0530

    Update Request.swift

--- Request.swift
+++ Request.swift
@@ -6,3 +6,19 @@ class Request: NSObject {
     var dictQueryValues : [String : String] = [:]
     
 }
+
+/*
+//Obj - C Block Declaraion
+1. Block Declaration
+typedef void (^RequestComplitionBlock)(NSObject *resObj);
+
+typedef <RETURN_TYPE> (^CLAUSER_NAME) (PARAMETERS)
+=======
+2. Object Declaration of Block
+RequestComplitionBlock complitionBlock;
+=======
+3. Global Variable of Block use in Method
+-(void)uploadAllDataToServer :(RequestComplitionBlock) reqBlock { 
+    complitionBlock = reqBlock;
+}
+*/
